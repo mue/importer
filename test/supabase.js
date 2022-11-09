98d80586794182484d0a6fc4ebcb2c7b21d53408
Isaac <git@eartharoid.me>
2022-11-09 01:44:20 +0000

    add image categories function

--- test/supabase.js
+++ test/supabase.js
@@ -4,5 +4,6 @@ import { createClient } from '@supabase/supabase-js';
 config();
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SECRET);
-console.log(await supabase.rpc('getRandomImage').single());
-console.log(await supabase.rpc('getRandomQuote', { lang: 'English' }).single());
+console.log(await supabase.rpc('get_image_categories'));
+console.log(await supabase.rpc('get_random_image').single());
+console.log(await supabase.rpc('get_random_quote', { lang: 'English' }).single());
