198f0bf7f0c9dcece5838f84cd8805d0177bde28
Isaac <git@eartharoid.me>
2022-11-09 13:13:51 +0000

    more function tests

--- test/supabase.js
+++ test/supabase.js
@@ -5,5 +5,9 @@ config();
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SECRET);
 console.log(await supabase.rpc('get_image_categories'));
+console.log(await supabase.rpc('get_image_photographers'));
+console.log(await supabase.rpc('get_quote_languages'));
+
 console.log(await supabase.rpc('get_random_image').single());
-console.log(await supabase.rpc('get_random_quote', { lang: 'English' }).single());
+console.log(await supabase.rpc('get_random_image', { _category: 'plants' }).single());
+console.log(await supabase.rpc('get_random_quote').single());
