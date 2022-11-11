6a60b2db4e14ebecdb90c3247354ad50b57900ae
Isaac <git@eartharoid.me>
2022-11-11 17:01:03 +0000

    s-max-age

--- index.js
+++ index.js
@@ -210,7 +210,7 @@ for (const file of files) {
 					ACL: 'public-read',
 					Body: variants[variant],
 					Bucket: process.env.S3_BUCKET,
-					CacheControl: 'public, max-age=604800, stale-while-revalidate=86400, immutable',
+					CacheControl: 'public, max-age=604800, s-max-age=31536000, stale-while-revalidate=86400, immutable',
 					ContentMD5: createHash('md5').update(variants[variant]).digest('base64'),
 					ContentType: 'image/' + variant.split('.').pop(),
 					Key: variant,
